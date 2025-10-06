f213af3843b4fe5c23bd12f4b060971ce7c036da
MadhuriKothagorla <madhurikothagorla@gmail.com>
2025-10-06 23:08:01 +0530

    Add files via upload

---
+++ ArrayAlternateNumbers.java
@@ -0,0 +1,15 @@
+package com.madhu;
+
+public class ArrayAlternateNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,20,13,15,18,16,17};
+		for(int i=0;i<=a.length-1;i=i+2)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
