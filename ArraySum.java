ca058857fef5020bba332dfc1f7919f6dac14fad
MadhuriKothagorla <madhurikothagorla@gmail.com>
2025-10-06 23:11:53 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,17 @@
+package com.madhu;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,20,30,40};
+		int sum=0;
+		for(int num:a)
+		{
+			sum+=num;
+		}
+		System.out.println("Array sum:"+sum);
+
+	}
+
+}
