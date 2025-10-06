e0c8ada319870fca6ba29a4d5fb14e637b08e4db
MadhuriKothagorla <madhurikothagorla@gmail.com>
2025-10-06 23:09:34 +0530

    Add files via upload

---
+++ Arrayeven.java
@@ -0,0 +1,19 @@
+package com.madhu;
+
+public class Arrayeven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,20,13,15,18};
+				for(int i=0;i<=a.length-1;i++)
+				{
+					if(a[i]%2==0)
+					{
+						System.out.println(a[i]);
+					}
+				}
+		}
+
+	}
+
+
