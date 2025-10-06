e255f8901948dac8114ac837d03565cca96c9eb4
MadhuriKothagorla <madhurikothagorla@gmail.com>
2025-10-06 23:05:25 +0530

    Add files via upload

---
+++ AddArrays.java
@@ -0,0 +1,33 @@
+package com.madhu;
+
+public class AddArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]a= {1,2,3};
+		int[]b= {10,40,50};
+		int[]c=new int[a.length];
+		
+		for(int i=0;i<a.length;i++)
+		{
+			c[i]=a[i]+b[i];
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		for(int i=0;i<c.length;i++)
+		{
+			System.out.println(c[i]);
+		}
+
+	}
+
+}
